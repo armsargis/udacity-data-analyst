f7aff2b88bd4cc4c8d48ac8951e821bd6831c4de
Sargis Harutyunyan <sargis.harutyunyan@webbfontaine.com>
2021-05-21 12:24:14 +0400

    Use coeff based on median to compute missing values for yerevan results

--- weather-trends/main.py
+++ weather-trends/main.py
@@ -1,18 +1,15 @@
+import math
+
 import pandas as pd
 from pandas import DataFrame
 
 WINDOW_SIZE = 7
 
 
-def compute_moving_averages(file: str, post_processor=lambda x: x) -> DataFrame:
-    data: DataFrame = post_processor(pd.read_csv(file))
+def compute_moving_averages(df: DataFrame, post_processor=lambda x: x) -> DataFrame:
+    data: DataFrame = post_processor(df)
     data['moving_avg_temp'] = data['avg_temp'].rolling(window=WINDOW_SIZE).mean()
     return data
-
-
-def fill_no_data_cells(df: DataFrame) -> DataFrame:
-    median = df.median()
-    return df.fillna(median)
 
 
 def merge(left: DataFrame, right: DataFrame) -> DataFrame:
@@ -20,10 +17,30 @@ def merge(left: DataFrame, right: DataFrame) -> DataFrame:
     return joined.iloc[WINDOW_SIZE - 1:]
 
 
+def align_yerevan_results(yerevan_data: DataFrame, global_data: DataFrame) -> DataFrame:
+    corr = yerevan_data['avg_temp'].median() / global_data['avg_temp'].median()
+
+    def correlate(row):
+        if math.isnan(row['avg_temp_y']):
+            return row['avg_temp_x'] * corr
+        else:
+            return row['avg_temp_y']
+
+    aligned_results = pd.merge(global_data, yerevan_data, how='left', on=['year'])
+    aligned_results['avg_temp'] = aligned_results.apply(lambda row: correlate(row), axis=1)
+    aligned_results.drop(['avg_temp_x', 'avg_temp_y'], axis=1, inplace=True)
+
+    return aligned_results
+
+
 if __name__ == '__main__':
-    yerevan_averages = compute_moving_averages('csv/yerevan-results.csv', fill_no_data_cells)
-    global_averages = compute_moving_averages('csv/global-results.csv',
-                                              lambda df: df[(df.year >= 1780) & (df.year <= 2013)])
+    yerevan_results = pd.read_csv('csv/yerevan-results.csv')
+    global_results = pd.read_csv('csv/global-results.csv')
+
+    yerevan_aligned_results = align_yerevan_results(yerevan_results, global_results)
+
+    global_averages = compute_moving_averages(global_results)
+    yerevan_averages = compute_moving_averages(yerevan_aligned_results)
 
     merged = merge(yerevan_averages, global_averages)
     merged.rename({'moving_avg_temp_x': 'Yerevan', 'moving_avg_temp_y': 'Global'}, axis=1, inplace=True)
